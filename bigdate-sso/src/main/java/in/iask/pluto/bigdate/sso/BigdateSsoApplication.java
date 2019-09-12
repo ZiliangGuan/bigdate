775313be2fcdf19022d96339692ea9fbdb31bcc5
Ziliang Guan <ZLGuan@vip.qq.com>
2019-09-12 23:01:17 +0800

    Upload by Ziliang Guan

---
+++ bigdate-sso/src/main/java/in/iask/pluto/bigdate/sso/BigdateSsoApplication.java
@@ -0,0 +1,13 @@
+package in.iask.pluto.bigdate.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdateSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigdateSsoApplication.class, args);
+    }
+
+}
